17d70a6135a615b175997089a10a21c257fb67bd
nicholasnet <nirmalp@hotmail.com>
2023-05-18 13:24:04 -0400

    Fixed the backend 404 error for frontend related path. (#9)

---
+++ backend/main/java/com/ideasbucket/tansen/configuration/CustomWebFilter.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the Tansen project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+package com.ideasbucket.tansen.configuration;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomWebFilter implements WebFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        final String basePath = exchange.getRequest().getPath().contextPath().value();
+        final String path = exchange.getRequest().getPath().pathWithinApplication().value();
+
+        if (!path.startsWith("/api") && !path.startsWith("/assets")) {
+            return chain.filter(
+                exchange
+                    .mutate()
+                    .request(exchange.getRequest().mutate().path(basePath + "/").contextPath(basePath).build())
+                    .build()
+            );
+        }
+
+        return chain.filter(exchange);
+    }
+}
